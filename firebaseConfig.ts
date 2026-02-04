ed2e665e6ff6d3e7d2acfcd0a3e8b71d7f86fc09
sakuni <saakfernandz@gmail.com>
2026-02-04 16:17:30 +0530

    Initial project setup

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8F-HgI3Xpi2WRtT1bzzVAFbaOa3j23fI",
+  authDomain: "mindease-cd0e1.firebaseapp.com",
+  projectId: "mindease-cd0e1",
+  storageBucket: "mindease-cd0e1.firebasestorage.app",
+  messagingSenderId: "854674232878",
+  appId: "1:854674232878:web:c2e39be8fb7b496e58f1bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
